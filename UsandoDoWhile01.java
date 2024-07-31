ecb6995187c3bd5284759c64c0247b83664bb317
Marcela <marcela.maciel2408@gmail.com>
2024-07-31 16:16:07 -0300

    Add files via upload

---
+++ UsandoDoWhile01.java
@@ -0,0 +1,14 @@
+
+public class UsandoDoWhile01 {
+
+	public static void main(String[] args) {
+		int cta = 0;
+		
+		do {
+			System.out.println("Eu sou o Do While");
+			cta++;
+		} while(cta<=10);
+
+	}
+
+}
