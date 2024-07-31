a5aeea474be68b8386491f7d8d7342fab5ceccac
Marcela <marcela.maciel2408@gmail.com>
2024-07-31 14:31:07 -0300

    Add files via upload

---
+++ UsandoWhile02.java
@@ -0,0 +1,17 @@
+
+public class UsandoWhile02 {
+
+	public static void main(String[] args) {
+		int cta = 0;
+		int num = 7;
+		int total = 0;
+		
+		while(cta<=10) {
+			total = num*cta;
+			System.out.println(num + "x" + cta + " = " + total);
+			cta++;
+		}
+
+	}
+
+}
